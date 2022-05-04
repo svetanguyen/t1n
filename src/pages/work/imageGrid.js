88486e2e9cad815f2bc124292cfccc3b116f7741
svetanguyen <matematicaforeber@gmail.com>
2022-05-04 16:59:48 +0700

    create work page

---
+++ src/pages/work/imageGrid.js
@@ -0,0 +1,43 @@
+export default function Grid() {
+    return (
+        <div className="container mx-auto">
+            <div className="image-grid lg:grid lg:-mx-2 grid-cols-2 px-4 pb-10 lg:pb-21 lg:px-11">
+                <div className="image-grid__item col-start-1 col-span-1 py-2 h-full lg:p-2">
+                    <img
+                        src="/images/work.jpg"
+                        alt="portfolio"
+                        className="w-full max-h-738 h-full object-cover"
+                    />
+                </div>
+                <div className="image-grid__item h-full col-start-2 col-span-1 py-2 lg:p-2">
+                    <img
+                        src="/images/work-2.jpg"
+                        alt="portfolio"
+                        className="w-full max-h-738 h-full object-cover"
+                    />
+                </div>
+                <div className="image-grid__item py-2 lg:p-2 col-span-2">
+                    <img
+                        src="/images/work-3.jpg"
+                        alt="portfolio"
+                        className="w-full max-h-738 h-full object-cover"
+                    />
+                </div>
+                <div className="image-grid__item py-2 lg:p-2">
+                    <img
+                        src="/images/work-4.jpg"
+                        alt="portfolio"
+                        className="w-full max-h-738 h-full object-cover"
+                    />
+                </div>
+                <div className="image-grid__item py-2 lg:p-2">
+                    <img
+                        src="/images/work-5.jpg"
+                        alt="portfolio"
+                        className="w-full max-h-738 h-full object-cover"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/work/index.js
+++ src/pages/work/index.js
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
 import Works from '../../components/Works'
+import LinksList from '../../components/LinksList'
+import Grid from './imageGrid'
+import { DRIBBLE, BEHANCE } from '../../constants/links'
 
 export default function Work() {
     useEffect(() => {
         document.title = 'My craft';
       }, []);
+      const links = [
+          {name: 'My dribble', link: DRIBBLE},
+          {name: 'My behance', link: BEHANCE}
+      ]
 
     return (
-        <div className=" bg-dark-100 lg:pt-20">
+        <div className=" bg-dark-100 lg:pt-20 pt-10">
             <Works/>
+            <div className="pt-6 lg:pt-14">
+                <LinksList links={links} color='white' center={true} />
+            </div>
+            <Grid />
         </div>
     )
 }
