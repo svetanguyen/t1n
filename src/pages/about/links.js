13e678a9b1ffbb86a748188337cd834cca17d670
svetanguyen <matematicaforeber@gmail.com>
2022-06-01 14:34:45 -0400

    set min hegith

--- src/pages/about/links.js
+++ src/pages/about/links.js
@@ -9,7 +9,7 @@ export default function Links() {
         {name: 'Facebook', link: FACEBOOK}
     ]
     return (
-        <div className="bg-gradient-top-60 lg:gradient-top-50">
+        <div className="bg-gradient-top-60 lg:gradient-top-50 min-h-screen">
             <div className="pb-16 lg:pb-22 px-4 lg:px-11 pt-32 lg:pt-21 lg:grid grid-rows-2 grid-cols-2">
                 <div className="pb-12  text-center lg:text-left row-start-1 row-span-1 col-span-1 lg:pb-20">
                     <a href="/" className="text-gray-100 font-extended underline text-56 lg:text-100">Resume</a>
